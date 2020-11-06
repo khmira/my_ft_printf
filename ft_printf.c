7654c8be31f5ee1c85cf422394604a4a98059855
khmira <amine.badidi@hotmail.com>
2020-11-06 19:41:05 +0100

    il faut traiter presgit add .

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: abadidi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/06 01:48:08 by abadidi           #+#    #+#             */
-/*   Updated: 2020/11/06 14:14:19 by abadidi          ###   ########.fr       */
+/*   Updated: 2020/11/06 18:56:49 by abadidi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,15 @@ int	ft_printf(const char *format, ...)
 	va_start(args, format);
 	while (*text)
 	{
-		if (0)
+		if (*text == '%')
 		{
-			int a = 1;//debut de n'importe quoi
-			int b = 1;//encore n'importe quoi
-			return(a+b);//fin de n'importe quoi
+			text++;
+			if (*text == '.')//*.*
+			{
+				int a = 1;//debut de n'importe quoi
+				int b = 1;//encore n'importe quoi
+				return(a+b);//fin de n'importe quoi
+			}
 		}
 		else {
 			ft_putchar(*text);//cas sans %
--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 int main()
 {
 	int a = ft_printf("\nfuck you\n");printf("\n");
-	int b = printf("\nfuck you\n");printf("\n");
+	int b = printf("\nfuck you \n%.da\n",-4,1);printf("\n");
 
 	printf("%d   |  %d\n", a, b);
 
